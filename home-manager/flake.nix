705a848630be92ae9f55043681513cd568e7f0bb
Wes Florence <hello@wesflorence.com>
2023-10-17 22:51:18 -0700

    adding darwin system

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,29 +15,40 @@
     };
   };
 
-  outputs = { nixpkgs, home-manager, nix-search-cli, ... }:
-    let
-      system = "x86_64-linux";
-      # Define an overlay that adds nix-search-cli to the package set
-      overlay = final: prev: {
-        nix-search-cli = nix-search-cli.packages.${system}.nix-search;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    home-manager,
+    nix-search-cli,
+    ...
+  }:
+  flake-utils.lib.eachSystem ["x86_64-linux" "aarch64-darwin" ]
+    (system:
+      let
+        # Define an overlay that adds nix-search-cli to the package set
+        overlay = final: prev: {
+          nix-search-cli = nix-search-cli.packages.${system}.nix-search;
+        };
 
-      # Apply the overlay to get a modified pkgs
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ overlay ];
-      };
-    in {
-      homeConfigurations."wesley" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+        # Apply the overlay to get a modified pkgs
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
+      in {
+        packages.homeConfigurations = {
+          wesley = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
+            # Specify your home configuration modules here, for example,
+            # the path to your home.nix.
+            modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-    };
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+          };
+        };
+      }
+    );
 }
